bddaddd37fea2decee3065f72044327ca74f826a
Henrique Souza <72718207+henrique-souza@users.noreply.github.com>
2021-08-29 00:37:04 -0300

    Update operacoes.py

--- Sequential Structure/operacoes.py
+++ Sequential Structure/operacoes.py
@@ -1,7 +1,5 @@
-print("First number: ")
-first_number = int(input())
-print("\nSecond Number: ")
-second_number = int(input())
+first_number = int(input("First number: "))
+second_number = int(input("\nSecond Number: "))
 
 soma = first_number + second_number
 subtracao = first_number - second_number
@@ -10,9 +8,9 @@ divisao = first_number / second_number
 exponenciacao = first_number ** second_number
 resto_de_divisao = first_number % second_number
 
-print(soma)
-print(subtracao)
-print(multiplicacao)
-print(divisao)
-print(exponenciacao)
-print(resto_de_divisao)
+print("\n\nResultado da Soma: " + str(soma))
+print("\nResultado da Subtração: " + str(subtracao))
+print("\nResultado da Multiplicação: " + str(multiplicacao))
+print("\nResultado da Divisão: " + str(divisao))
+print("\nResultado da Exponenciação: " + str(exponenciacao))
+print("\nResultado do resto da Divisão: " + str(resto_de_divisao))
