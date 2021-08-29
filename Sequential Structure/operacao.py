6c50c51a88d60b9493e84039053f58b413175ce4
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-08-28 23:56:10 -0300

    Initial commit

---
+++ Sequential Structure/operacao.py
@@ -0,0 +1,18 @@
+print("First number: ")
+first_number = int(input())
+print("\nSecond Number: ")
+second_number = int(input())
+
+soma = first_number + second_number
+subtracao = first_number - second_number
+multiplicacao = first_number * second_number
+divisao = first_number / second_number
+exponenciacao = first_number ** second_number
+resto_de_divisao = first_number % second_number
+
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+print(exponenciacao)
+print(resto_de_divisao)
