29a995fd727a22d1170abd51a363cdbb7400e2de
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-11 16:09:29 -0300

    Add files via upload

---
+++ Conditional Structures/microsoft_challenge_03.py
@@ -0,0 +1,21 @@
+''' # testando a função type(), que expressa que tipo
+# de dados estamos trabalhando
+
+print(type('Olá mundo!'))
+print(type(7))
+
+print(type(True))
+print(type(False))
+
+print(type('True'))
+print(type('False'))'''
+
+'''#agora irei testar a função bool()
+
+print(bool('True'))
+print(bool('False'))
+print(bool('')) # cadeia de carcteres vazia, logo, False
+print(bool(' '))
+print(bool('Olá mundo!'))'''
+
+#parei na etapa 04
