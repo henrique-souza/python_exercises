672ed1b58724f58ca02c8ba060d8224e7d213876
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-11 17:35:35 -0300

    Update microsoft_challenge_04.py

--- Conditional Structures/microsoft_challenge_04.py
+++ Conditional Structures/microsoft_challenge_04.py
@@ -1,21 +1,49 @@
-''' # testando a função type(), que expressa que tipo
-# de dados estamos trabalhando
+'''
+#programa 01
 
-print(type('Olá mundo!'))
-print(type(7))
+#testando a função type(), que expressa que tipo de dados estou trabalhando
 
-print(type(True))
-print(type(False))
+print(type('Olá mundo!')) #imprime str
+print(type(7)) #imprime int
 
-print(type('True'))
-print(type('False'))'''
+print(type(True)) #imprime bool
+print(type(False)) #imprime bool
 
-'''#agora irei testar a função bool()
+print(type('True')) #imprime str
+print(type('False')) #imprime str
+
+#programa 02
+
+#agora irei testar a função bool()
 
 print(bool('True'))
 print(bool('False'))
-print(bool('')) # cadeia de carcteres vazia, logo, False
+print(bool('')) #cadeia de carcteres vazia, logo, False
 print(bool(' '))
-print(bool('Olá mundo!'))'''
+print(bool('Olá mundo!'))
 
-#parei na etapa 04
+#programa 03
+
+print(bool(7))
+print(bool(1))
+print(bool(0)) #este valor não sendo diferente de 0, imprimirá False
+print(bool(-1))
+
+#programa 04
+
+print(1 + 1 == 3) #vai imprimir um False    
+print(1 + 1 == 2) #vai imprimir um True 
+
+#programa 05
+
+print(3 == 4)
+print(3 != 4)
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+
+'''
+#programa 06
+
+
