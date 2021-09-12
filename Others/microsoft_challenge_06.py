163095e38f6f0975cd024b1772b137cce82cf07c
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-12 00:29:26 -0300

    Update microsoft_challenge_06.py

--- Others/microsoft_challenge_06.py
+++ Others/microsoft_challenge_06.py
@@ -1,1 +1,26 @@
-a = 1
+'''
+#programa 01
+
+primeira_string = 'Uma string literal'
+segunda_string = "Uma string literal"
+
+print(segunda_string == primeira_string) #ira imprimir um True
+
+#programa 02
+
+terceira_string = ('Uma única sequência literal citada com uma " citação dupla')
+quarta_string = ("Duas sequências literais citadas com ' uma única citação")
+
+print(terceira_string)
+print(quarta_string)
+'''
+
+#programa 03
+
+quinta_string = ('')
+sexto_string = ("")
+setimo_string = ('')
+oitavo_string = ('')
+
+print(quinta_string)
+# parei na etapa 4
