f15abbb5aa102db329011f2966ff8d2a82f9a375
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-14 12:03:02 -0300

    Update microsoft_challenge_06.py

--- Strings/microsoft_challenge_06.py
+++ Strings/microsoft_challenge_06.py
@@ -12,8 +12,6 @@ quarta_string = ("Duas sequências literais citadas com ' uma única citação")
 print(terceira_string)
 print(quarta_string)
 
-'''
-
 #programa 03
 
 quinta_string = ('Uma sequência literal citada com uma \' unica citação escapada')
@@ -25,3 +23,22 @@ print(quinta_string)
 print(sexta_string)
 print(setima_string)
 print(oitava_string)
+
+#programa 04
+https://www.onlinegdb.com/#tab-stdin
+nona_string = r"Uma sequência literal com uma \n nova linha impressa crua"
+print(nona_string)
+
+'''
+#programa 05
+
+decima_string = '''Uma sequência literal
+em mais de uma linha
+às vezes conhecida como cadeia de caracteres verbatim'''
+decimaPrimeira_string = """Outra sequência literal
+    em mais de uma linha
+usando citação dupla"""
+
+print(decima_string)
+print ('\n')
+print(decimaPrimeira_string)
