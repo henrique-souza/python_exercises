b9b697051839ef4893f3d0c799873d8d91b955ea
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-14 15:11:24 -0300

    Create microsoft_challenge_07.py

---
+++ Strings/microsoft_challenge_07.py
@@ -0,0 +1,61 @@
+# caso queira testar os programas separadamente, adicione
+# comentários de apóstofos simples ''' e ''' ENTRE um programa e outro.
+#Exemplo abaixo:
+'''
+
+print('Programa X')
+código
+
+'''
+''' #excluir estes apóstofos ao terminar os exercicios
+print('Programa 01\n')
+
+mensagem = str.capitalize('primeira mensagem')
+print(mensagem)
+
+mensagem = 'segunda mensagem'.capitalize()
+print(mensagem)
+
+mensagem = 'terceira mensagem'
+print(mensagem.capitalize())
+print('\n')
+
+
+print('Programa 02\n')
+
+mensagem2 = 'ola mundo'
+print(mensagem2.lower())
+print(mensagem2.upper())
+
+mensagem2 = mensagem2.title()
+print(mensagem2)
+print(mensagem2.swapcase())
+print('\n')
+
+print('Programa 03\n')
+
+localizacao = 'Mississipi'
+print(localizacao.count('s'))
+print('\n')
+
+print('Programa 04\n')
+
+print(len('Quantos caracteres tem nesta cadeia de caracteres?'))
+print('\n')
+
+print('Programa 05\n')
+
+mensagem3 = 'racecar'
+print(mensagem3.startswith('r'))
+print(mensagem3.startswith('a'))
+print(mensagem3.startswith('ra'))
+
+print(mensagem3.endswith('r'))
+print(mensagem3.endswith('a'))
+print(mensagem3.endswith('ar')) #estes comandos irão retornar se 
+# a informação passada no parametro é True ou False
+print('\n')
+
+'''
+
+print('Programa 06\n')
