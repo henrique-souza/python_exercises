c5f8e4b1a2c024d1da12ba3cc3e3edb63fbceefe
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-14 12:25:17 -0300

    Update microsoft_challenge_06.py

--- Strings/microsoft_challenge_06.py
+++ Strings/microsoft_challenge_06.py
@@ -1,14 +1,18 @@
-'''
+#caso queira executar os programas separadamente, 
+# use três citaçções simples ''' na linha inicial e final ''' 
+# de cada "# programa x"
+
 #programa 01
 
 primeira_string = 'Uma string literal'
 segunda_string = "Uma string literal"
-print(segunda_string == primeira_string) #ira imprimir um True
+print(segunda_string == primeira_string) #irá imprimir um boolean: True
 
 #programa 02
 
 terceira_string = ('Uma única sequência literal citada com uma " citação dupla')
 quarta_string = ("Duas sequências literais citadas com ' uma única citação")
+print ('\n')
 print(terceira_string)
 print(quarta_string)
 
@@ -18,7 +22,7 @@ quinta_string = ('Uma sequência literal citada com uma \' unica citação escap
 sexta_string = ("Uma sequência literal citada com uma \" citação dupla")
 setima_string = ('Uma sequência literal com um \n caractere de linha')
 oitava_string = ('Uma sequência literal com um \t caractere de guia ou tabulação')
-
+print ('\n')
 print(quinta_string)
 print(sexta_string)
 print(setima_string)
@@ -27,25 +31,35 @@ print(oitava_string)
 #programa 04
 
 nona_string = r"Uma sequência literal com uma \n nova linha impressa crua"
+print ('\n')
 print(nona_string)
 
 
 #programa 05
 
 
-decima_string = ''' '''Uma sequência literal
+decima_string = '''Uma sequência literal
 em mais de uma linha
-às vezes conhecida como cadeia de caracteres verbatim''' '''
-# antes de executar a linha acima, favor remover três citações que estão
-# transoformando o dado da variável num comentário
+às vezes conhecida como cadeia de caracteres verbatim'''
+# para executar o "#programa 06" sem interferencias dos dados da
+# variavel "decima_string", favor adicionar comentários com 
+# citações simples ''' ANTES e DEPOIS ''' dos dados.
 decimaPrimeira_string = """Outra sequência literal
     em mais de uma linha
 usando citação dupla"""
-
+print ('\n')
 print(decima_string)
 print ('\n')
 print(decimaPrimeira_string)
 
-'''
+
 #programa 06
 
+primeiroNome = 'Conrad'
+segundoNome = 'Grant'
+terceiroNome = 'Bob'
+print ('\n')
+print(primeiroNome, segundoNome)
+print(primeiroNome, segundoNome, terceiroNome)
+print(primeiroNome, segundoNome, terceiroNome, sep='-')
+print(primeiroNome, segundoNome, terceiroNome, sep='-', end='.')
