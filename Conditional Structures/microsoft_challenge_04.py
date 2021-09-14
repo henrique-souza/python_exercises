a04687dc7ccb7fa662160ad055583faaed0dfa5d
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-14 12:33:05 -0300

    Update microsoft_challenge_04.py

--- Conditional Structures/microsoft_challenge_04.py
+++ Conditional Structures/microsoft_challenge_04.py
@@ -1,43 +1,50 @@
-# Remova estes primeiros apóstofos e coloque-os ANTES/ACIMA dos
-# comentários "# programa X" e execute as linhas que deseja testar
+# caso queira testar os programas separadamente, adicione
+# comentários de apóstofos simples ''' e ''' ENTRE um programa e outro.
+#Exemplo abaixo:
+'''
 
-''' 
-#programa 01
+print('Programa X')
+código
+
+'''
+
+print('Programa 01\n')
 
 #testando a função type(), que expressa que tipo de dados estou trabalhando
 
 print(type('Olá mundo!')) #imprime str
 print(type(7)) #imprime int
-
 print(type(True)) #imprime bool
 print(type(False)) #imprime bool
-
 print(type('True')) #imprime str
 print(type('False')) #imprime str
+print('\n')
 
-#programa 02
+print('Programa 02\n')
 
 #agora irei testar a função bool(), irá imprimir se os valores setados são True ou False
-
 print(bool('True'))
 print(bool('False'))
 print(bool('')) #cadeia de carcteres vazia, logo, False
 print(bool(' '))
 print(bool('Olá mundo!'))
+print('\n')
 
-#programa 03
+print('Programa 03\n')
 
 print(bool(7))
 print(bool(1))
 print(bool(0)) #este valor não sendo diferente de 0, imprimirá False
 print(bool(-1))
+print('\n')
 
-#programa 04
+print('Programa 04\n')
 
 print(1 + 1 == 3) #vai imprimir um False    
 print(1 + 1 == 2) #vai imprimir um True 
+print('\n')
 
-#programa 05
+print('Programa 05\n')
 
 #vai imprimir quais operações são False ou True
 print(3 == 4)
@@ -46,8 +53,9 @@ print(3 > 4)
 print(3 < 4)
 print(3 >= 4)
 print(3 <= 4)
-'''
-#programa 06
+print('\n')
+
+print('Programa 06\n')
 
 primeiro_numero = 5
 segundo_numero = 0
