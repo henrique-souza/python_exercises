f3c3387230793f8be0e0c346b31ca0db8d9e237f
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-14 10:44:20 -0300

    Update microsoft_challenge_06.py

--- Strings/microsoft_challenge_06.py
+++ Strings/microsoft_challenge_06.py
@@ -3,24 +3,25 @@
 
 primeira_string = 'Uma string literal'
 segunda_string = "Uma string literal"
-
 print(segunda_string == primeira_string) #ira imprimir um True
 
 #programa 02
 
 terceira_string = ('Uma única sequência literal citada com uma " citação dupla')
 quarta_string = ("Duas sequências literais citadas com ' uma única citação")
-
 print(terceira_string)
 print(quarta_string)
+
 '''
 
 #programa 03
 
-quinta_string = ('')
-sexto_string = ("")
-setimo_string = ('')
-oitavo_string = ('')
+quinta_string = ('Uma sequência literal citada com uma \' unica citação escapada')
+sexta_string = ("Uma sequência literal citada com uma \" citação dupla")
+setima_string = ('Uma sequência literal com um \n caractere de linha')
+oitava_string = ('Uma sequência literal com um \t caractere de guia ou tabulação')
 
 print(quinta_string)
-# parei na etapa 4
+print(sexta_string)
+print(setima_string)
+print(oitava_string)
