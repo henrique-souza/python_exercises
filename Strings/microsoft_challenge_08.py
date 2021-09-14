49d0bc8184027ea0357ba2183033e62780e28d8e
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-14 16:50:09 -0300

    Create microsoft_challenge_08.py

---
+++ Strings/microsoft_challenge_08.py
@@ -0,0 +1,15 @@
+# caso queira testar os programas separadamente, adicione
+# comentários de apóstofos simples ''' e ''' ENTRE um programa e outro.
+#Exemplo abaixo:
+'''
+
+print('Programa X')
+código
+
+'''
+#parei na unidade 4, usos de 'format()'
+
+print('Programa 01\n')
+
+
+print('\n')
