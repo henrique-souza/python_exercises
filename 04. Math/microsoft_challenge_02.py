49340170691ea5e4df5422581790323dce506aca
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-17 15:43:03 -0300

    Update microsoft_challenge_02.py

--- 04. Math/microsoft_challenge_02.py
+++ 04. Math/microsoft_challenge_02.py
@@ -10,5 +10,26 @@ código
 
 print('Programa 01')
 
+valor_numerico = '7'
+print(valor_numerico.isnumeric())
 
+valor_string = 'Henrique'
+print(valor_string.isnumeric())
+print('\n')
+
+
+print('Programa 02')
+
+primeiro_valor = input('Primeiro numero: ')
+segundo_valor = input('Segundo numero: ')
+
+if primeiro_valor.isnumeric() == False or segundo_valor.isnumeric() == False:
+    print('O valor digitado não é um número.')
+    exit()
+    
+primeiro_valor = int(primeiro_valor)
+segundo_valor = int(segundo_valor)
+
+soma = primeiro_valor + segundo_valor
+print('Soma dos valores: ' + str(soma))
 print('\n')
