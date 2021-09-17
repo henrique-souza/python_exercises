e6ab87041a9290e86abbf2824b17b77f993e64ee
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-17 14:40:47 -0300

    Create microsoft_challenge_02.py

---
+++ 04. Math/microsoft_challenge_02.py
@@ -0,0 +1,14 @@
+# caso queira testar os programas separadamente, adicione
+# comentários de apóstofos simples ''' e ''' ENTRE um programa e outro.
+# Exemplo abaixo:
+
+'''
+print('Programa X')
+código
+
+'''
+
+print('Programa 01')
+
+
+print('\n')
