7fc88e512b5f4e46834e4a3a337a9d7c9c504612
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-21 16:59:25 -0300

    Create fahrenheit_to_celsius.py

---
+++ 04. Math/fahrenheit_to_celsius.py
@@ -0,0 +1,15 @@
+fahrenheit = input('What is the temperature in Fahrenheit?  ')
+
+if fahrenheit.isnumeric() == False:
+    print('Input is not a number.')
+    exit()
+    
+fahrenheit = int(fahrenheit)
+
+celsius = (fahrenheit - 32) * 5/9
+
+celsius = int(celsius)
+
+print('Temperature in celsius is ' + str(celsius) + '.')
+
+print('\n')
