f05af5ea4fcd43e67fd786752c9b69c106e615da
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-21 17:44:44 -0300

    Update simple_calculator.py

--- 04. Math/simple_calculator.py
+++ 04. Math/simple_calculator.py
@@ -1,1 +1,18 @@
+'''print('Simple calculator!')
 
+first_number = input('First number? ')
+
+operation = input('Operation? ')
+
+second_number = input('Second number? ')
+
+if operation == 'p':
+    
+    product = f'{first_number}, {second_number}, {first_number * second_number}'
+    
+    print(product)
+    
+    exit()
+
+
+print('\n')'''
