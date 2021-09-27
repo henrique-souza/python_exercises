6c0b08d9df1f5c174869f95bde5760dc003ff4d9
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-27 18:00:20 -0300

    Update microsoft_exercise_01.py

--- 06. Iteration/microsoft_exercise_01.py
+++ 06. Iteration/microsoft_exercise_01.py
@@ -4,8 +4,9 @@ roll = 0
 count = 0
 
 while roll != 5:
-  count = count + 1
-  roll = random.randint(1, 5)
-  print(roll)
+  count = count + 1 # count faz a conta de quantas vezes os números foram
+  # sortidos até chegar o número 5
+  roll = random.randint(1, 5) # sorteia os números de 1 a 5
+  print(roll) # joga na tela os números de 1 a 5, até que apareça 5.
 
 print(f'It took {count} rolls to roll a 5!')
