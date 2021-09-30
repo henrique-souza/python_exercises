afe546379b5dc5d54e3eecaceb682bebbf607b30
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-30 11:35:05 -0300

    Update microsoft_exercise_01.py

--- 06. Iteration/microsoft_exercise_01.py
+++ 06. Iteration/microsoft_exercise_01.py
@@ -3,13 +3,15 @@ import random
 roll = 0
 count = 0
 
-while roll != 5: # enquanto roll for diferente de 5
-    print(f'\nSorteio {count + 1}')  # joga na tela os números de 1 a 5, até que apareça 5.
-    count = count + 1  
-    ''' a variável count faz a conta de quantas vezes
-    os números foram sortidos até chegar o número 5
-    e depois exibe o total na linha 12 '''
-    roll = random.randint(1, 5)  # sorteia os números de 1 a 5
-    print(f'Resultado: {roll}')  # joga na tela os números de 1 a 5, até que apareça 5.
+# iniciando o laço 'roll' enquanto os dados forem diferentes de 5
+while roll != 5:
+    print(f'\nSorteio {count + 1}')
+    # joga na tela os números de 1 a 5, até que apareça 5.
+    count = count + 1
+    # 'count' conta de quantas vezes os números foram sortidos
+    roll = random.randint(1, 5)
+    # sorteia os números de 1 a 5
+    print(f'Resultado: {roll}')
+    # joga na tela os números de 1 a 5, até que apareça 5.
 print(f'\nDepois de {count} jogadas, conseguimos obter um 5!')
 print('\n')
