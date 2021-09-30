81956a276f89ad092da536a026155c3230f1624c
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-09-30 11:45:57 -0300

    Update microsoft_exercise_01.py

--- 06. Iteration/microsoft_exercise_01.py
+++ 06. Iteration/microsoft_exercise_01.py
@@ -5,13 +5,13 @@ count = 0
 
 # iniciando o laço 'roll' enquanto os dados forem diferentes de 5
 while roll != 5:
+    # joga na tela os números de 1 a 5, até que apareça 5
     print(f'\nSorteio {count + 1}')
-    # joga na tela os números de 1 a 5, até que apareça 5.
+    # 'count' conta quantas vezes os números foram sortidos
     count = count + 1
-    # 'count' conta de quantas vezes os números foram sortidos
-    roll = random.randint(1, 5)
     # sorteia os números de 1 a 5
+    roll = random.randint(1, 5)
+    # printa o resultado randomico de 'roll'
     print(f'Resultado: {roll}')
-    # joga na tela os números de 1 a 5, até que apareça 5.
 print(f'\nDepois de {count} jogadas, conseguimos obter um 5!')
 print('\n')
