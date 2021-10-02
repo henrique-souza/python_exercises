aa4d19c13c7659e18d2f8e9aaf09fd7770089000
Henrique Souza <h.s.s_henrique@hotmail.com>
2021-10-02 12:15:50 -0300

    Update and rename while.py to game_while.py

---
+++ 06. Iteration/game_while.py
@@ -0,0 +1,27 @@
+import random
+
+# este programa é como um jogo que sorteia de números de 1 a 5
+# até que se obtenha um 5. Ganha quem sortear o número 5 primeiro.
+# podem ser mais de um jogador, desde que respeite-se a ordem.
+roll = 0
+count = 0
+
+print('O primeiro que sortear um 5 vence!!')
+# iniciando o laço enquanto o valor da variável 'roll' for diferente de 5
+while roll != 5:
+    print('\n')
+    name = input('Digite seu nome ou \'q\' para sair do jogo:  ')
+    if name == 'q':
+        break
+    # se a variavel 'count' começa com 0, a partir deste print começara com 1
+    # contando assim a quantidade de sorteios corretamente
+    print(f'Sorteio {count + 1} - {name}')
+    # 'count' conta quantas vezes os números foram sortidos
+    count = count + 1
+    # sorteia os números de 1 a 5
+    roll = random.randint(1, 5)
+    # printa o resultado randomico de 'roll' até que apareça 5
+    print(f'{name} sorteou: {roll}')
+else:
+    print(f'{name} Ganhou!!!')
+print(f'\nO jogo sorteou {count} números!')
