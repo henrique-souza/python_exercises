9a14a08a53097822c79ad5377a6cc6cbabfb154a
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2022-11-11 00:07:49 -0300

    update python exercises

---
+++ 07. Data Structure/a2_modelagem.py
@@ -0,0 +1,32 @@
+# Prob de Menina = Prob de Menino = 1/2 ou 0,5 - Menina é 0 e Menino é 1
+# Exercicio 01
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+coluna = range(1)
+linha = list(10000)
+dados = np.random.random(10001)
+tabela = pd.DataFrame(data=dados, index=linha, columns=coluna)
+
+
+
+# creating the dataset
+data = {'%10': 20, '%100': 15, '%1000': 30,
+        '%10000': 35}
+courses = list(data.keys())
+values = list(data.values())
+
+fig = plt.figure(figsize=(10, 5))
+
+# creating the bar plot
+plt.bar(courses, values, color='blue',
+        width=0.4)
+
+plt.xlabel("aaaaaaaaa")
+plt.ylabel("bbbbbbbbbbbb")
+plt.title("Probabilidade de nascimento de Meninos ou Meninas")
+plt.show()
+
+# vou ver se faço um método que retorna varios dados aleatorios como queremos
+# e ai vai gravando, não gravando apenas um...
---
+++ 07. Data Structure/ms_challenge1.py
@@ -0,0 +1,5 @@
+import random
+# funções
+
+print(random.random())#Gera um número aleatório entre  0 e 1
+
