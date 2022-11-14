3c274c88cacc526c22b00a0fd4cdc5d7c2ee1459
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2022-11-14 11:30:11 -0300

    update python exercises

--- 07. Data Structure/a2_modelagem.py
+++ 07. Data Structure/a2_modelagem.py
@@ -5,46 +5,38 @@ import pandas as pd
 
 pd.options.display.float_format = '{:,.1f}'.format
 
-coluna = [0]
-linha = range(10000)
-dados = np.round(np.random.random(10000), 1)  # randomiza de 0 a 1
-tabela = pd.DataFrame(data=dados, index=linha, columns=coluna)
+coluna = [0]  # apenas uma coluna
+linha = range(10000)  # 10.000 linhas
+dados = np.round(np.random.random(10000), 1)  # cada linha recebe dados randomizados de 0 a 1
+tabela = pd.DataFrame(data=dados, index=linha, columns=coluna)  # confecção da tabela
 
 # retorna True se os valores contidos no "[indice 0 a 10, coluna]" forem maiores que 0,5
-n10_meninas = 0.5 < tabela.iloc[0:10, 0]
+n10_meninas = (0.5 < tabela.iloc[0:10, 0])
 
 # retorna True se os valores contidos no "[indice 0 a 10, coluna]" forem menores ou iguais a 0,5
 n10_meninos = 0.5 >= tabela.iloc[0:10, 0]
 
-# retorna True se os valores contidos no "[indice 0 a 100, coluna]" forem maiores que 0,5
 n100_meninas = 0.5 < tabela.iloc[0:100, 0]
-
-# retorna True se os valores contidos no "[indice 0 a 100, coluna]" forem menores ou iguais a 0,5
 n100_meninos = 0.5 >= tabela.iloc[0:100, 0]
 
-# retorna True se os valores contidos no "[indice 0 a 1000, coluna]" forem maiores que 0,5
 n1000_meninas = 0.5 < tabela.iloc[0:1000, 0]
-
-# retorna True se os valores contidos no "[indice 0 a 1000, coluna]" forem menores ou iguais a 0,5
 n1000_meninos = 0.5 >= tabela.iloc[0:1000, 0]
 
-# retorna True se os valores contidos no "[indice 0 a 10000, coluna]" forem maiores que 0,5
 n10000_meninas = 0.5 < tabela.iloc[0:10000, 0]
-
-# retorna True se os valores contidos no "[indice 0 a 10000, coluna]" forem menores ou iguais a 0,5
 n10000_meninos = 0.5 >= tabela.iloc[0:10000, 0]
 
-# print(f'Existem {sum(n10_meninas)} MENINAS no range de {n10_meninas.count()} registros.')
-# print(f'Existem {sum(n10_meninos)} MENINOS no range de {n10_meninos.count()} registros.')
-# print('\n')
-# print(f'Existem {sum(n100_meninas)} MENINAS no range de {n100_meninas.count()} registros.')
-# print(f'Existem {sum(n100_meninos)} MENINOS no range de {n100_meninos.count()} registros.')
-# print('\n')
-# print(f'Existem {sum(n1000_meninas)} MENINAS no range de {n1000_meninas.count()} registros.')
-# print(f'Existem {sum(n1000_meninos)} MENINOS no range de {n1000_meninos.count()} registros.')
-# print('\n')
-# print(f'Existem {sum(n10000_meninas)} MENINAS no range de {n10000_meninas.count()} registros.')
-# print(f'Existem {sum(n10000_meninos)} MENINOS no range de {n10000_meninos.count()} registros.')
+print(f'Existem {sum(n10_meninas)} MENINAS no range de {n10_meninas.count()} registros. \n '
+      f'Existem {sum(n10_meninos)} MENINOS no range de {n10_meninos.count()} registros. \n\n'
+      f'Existem {sum(n100_meninas)} MENINAS no range de {n100_meninas.count()} registros.\n'
+      f'Existem {sum(n100_meninos)} MENINOS no range de {n100_meninos.count()} registros.\n\n'
+      f'Existem {sum(n1000_meninas)} MENINAS no range de {n1000_meninas.count()} registros.\n'
+      f'Existem {sum(n1000_meninos)} MENINOS no range de {n1000_meninos.count()} registros.\n\n'
+      f'Existem {sum(n10000_meninas)} MENINAS no range de {n10000_meninas.count()} registros.\n'
+      f'Existem {sum(n10000_meninos)} MENINOS no range de {n10000_meninos.count()} registros.')
+
+# as linhas abaixo somam de quantas(os) meninas(os)
+# existem no range determinado de valores,
+# multiplicando por 100 e dividindo pelo total de meninas(os) no range
 
 # 10%
 fn10_meninas = sum((n10_meninas * 100) / n10_meninas.count())
